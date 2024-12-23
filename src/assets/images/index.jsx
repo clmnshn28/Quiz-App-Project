5355fbd67490ded518ca178341bdffe7c15ee301
Celmin Shane Quizon <celminshanequizon@gmail.com>
2024-12-23 23:02:30 +0800

    creating a Landing Page

--- src/assets/images/index.jsx
+++ src/assets/images/index.jsx
@@ -2,11 +2,12 @@ import loginBanner from './login_cover.png';
 import loginLogo from './login_logo.png';
 import logoutIcon from './logoutIcon.png';
 import defaultAvatar from './default-avatar.jpg';
-
+import landingCover from './landing_cover.png';
 
 export {
     loginBanner,
     loginLogo,
     logoutIcon,
     defaultAvatar,
+    landingCover,
 };
